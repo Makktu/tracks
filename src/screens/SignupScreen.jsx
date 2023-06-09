906f464f5d3b93729969de44876b790b71a88213
Makktu <johnmcna@gmail.com>
2023-06-09 18:58:18 +0100

    much progress

--- src/screens/SignupScreen.jsx
+++ src/screens/SignupScreen.jsx
@@ -1,10 +1,68 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, StatusBar, ImageBackground } from 'react-native';
+import { Text, Input, Button } from 'react-native-elements';
+import Spacer from '../components/Spacer';
 
-const SignupScreen = () => {
-  return <Text style={{ fontSize: 48 }}>SignupScreen</Text>;
+const image = {
+  uri: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1974&q=80',
 };
 
-const styles = StyleSheet.create({});
+const SignupScreen = ({ navigation }) => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <>
+      <View style={styles.backgroundStyle}>
+        {/* <StatusBar /> */}
+
+        <ImageBackground
+          source={image}
+          resizeMode='cover'
+          style={styles.image}
+          imageStyle={{ opacity: 0.6 }}
+        >
+          <View>
+            <Spacer>
+              <Text style={{ alignSelf: 'center', color: 'whitesmoke' }} h3>
+                Sign Up For Tracks
+              </Text>
+            </Spacer>
+            <Spacer>
+              <Input label='Email' />
+              <Input label='Password' />
+              <Button
+                title={'Sign Up'}
+                buttonStyle={{ backgroundColor: 'dodgerblue' }}
+                containerStyle={{ width: '50%', alignSelf: 'center' }}
+              />
+            </Spacer>
+          </View>
+        </ImageBackground>
+      </View>
+    </>
+  );
+};
+
+SignupScreen.navigationOptions = () => {
+  return {
+    headerShown: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    backgroundColor: 'black',
+    height: '100%',
+  },
+  image: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  content: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    marginBottom: 60,
+  },
+});
 
 export default SignupScreen;
--- src/screens/TrackDetailScreen.jsx
+++ src/screens/TrackDetailScreen.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
 const TrackDetailScreen = () => {
-  return <Text style={{ fontSize: 48 }}>TrackDetailScreen</Text>;
+  return <Text style={{ fontSize: 48 }}>Track-DetailScreen</Text>;
 };
 
 const styles = StyleSheet.create({});
